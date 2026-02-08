a57c8dfd87c47b70dd18e62e1e18ab37e5273432
jay <{{ .email }}>
2026-02-07 16:03:36 -0800

    style: show last updated in footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,12 +11,10 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const lastUpdated = new Date().toLocaleString(cfg.locale)
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
+        <p>Last updated: {lastUpdated}</p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
