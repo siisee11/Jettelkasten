f7ad93d72ba03973b6c5b53e26b2b7a7a9a508bf
jay <{{ .email }}>
2026-02-07 22:47:39 -0800

    feat: update about page

--- src/About.tsx
+++ src/About.tsx
@@ -4,40 +4,32 @@ export default function About() {
   return (
     <div className="page">
       <h1>About</h1>
-      <p>
-        I currently live in California and work at Stripe. I grew up in Ireland and previously
-        studied at MIT.
-      </p>
-      <p>Email: patrick@collison.ie.</p>
+      <p>I live in Suwon, South Korea, and I'm a co-founder of Wordbricks.</p>
+      <p>Email: siisee111@gmail.com.</p>
 
       <h2>Things I'm involved with and areas of interest:</h2>
       <ul>
         <li>
-          <strong>Economic growth.</strong> Stripe's core mission.
+          <strong>AI native programming.</strong>
         </li>
         <li>
-          <strong>Entrepreneurship.</strong> Stripe Atlas helps many new companies get started.
+          <strong>Bridging technology to the public.</strong>
         </li>
         <li>
-          <strong>Climate.</strong> Stripe Climate is the largest coalition of carbon removal purchasers
-          in the world.
+          <strong>Building agentic systems.</strong>
         </li>
         <li>
-          <strong>Housing production.</strong> CA YIMBY is pushing for important reform; so too is Ronan
-          Lyons. (See also the housing theory of everything.)
+          <strong>Books.</strong>
         </li>
         <li>
-          <strong>Books and ideas.</strong> Stripe Press and Works in Progress both publish on the theme
-          of economic and technological advancement. Tyler Cowen and I helped start Progress Studies.
-        </li>
-        <li>
-          <strong>Science.</strong> Arc Institute is a new way to do biomedical research. Fast Grants
-          distributed substantial amounts to scientists during the COVID pandemic.
+          <strong>Digital nomad.</strong>
         </li>
       </ul>
 
       <h2>Elsewhere</h2>
-      <p>pc on GitHub; patrickc on Twitter.</p>
+      <p>
+        GitHub: <a href="https://github.com/siisee11">https://github.com/siisee11</a>
+      </p>
     </div>
   );
 }
