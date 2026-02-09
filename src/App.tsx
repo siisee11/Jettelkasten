81b92ebb3771b6855defff97611a55867644d8f1
jay <{{ .email }}>
2026-02-09 04:38:57 -0800

    Show home sidebar on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,7 @@ const NotFound: React.FC = () => (
 const Home: React.FC<{ pages: Page[] }> = ({ pages }) => {
   return (
     <div className="page">
-      <HomeSidebar pages={pages} />
+      <HomeSidebar pages={pages} className="show-mobile" />
     </div>
   );
 };
